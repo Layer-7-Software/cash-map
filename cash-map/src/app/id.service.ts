a7829c1d1339821ba47477c7a7b678af2d60c33e
Adam Swanson <adam.swanson.98@gmail.com>
2021-07-07 17:55:18 -0400

    feat: add id service for generating ids

---
+++ cash-map/src/app/id.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IdService {
+
+  constructor() { }
+
+  newId(): string {
+    return uuidv4();
+  }
+
+}
