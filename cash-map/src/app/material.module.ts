62017116669542dc6c3ffbd04ff8c5549bf7f765
Adam Swanson <adam.swanson.98@gmail.com>
2021-07-13 21:12:46 -0400

    feat: add notification service

--- cash-map/src/app/material.module.ts
+++ cash-map/src/app/material.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   imports: [],
   exports: [
     MatButtonModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatSnackBarModule,
   ]
 })
 export class MaterialModule { }
